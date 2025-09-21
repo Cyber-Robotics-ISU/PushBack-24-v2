d94256b150382a0aa8af4686ee553d9248b51ec9
simon <a4coding@gmail.com>
2025-09-21 16:40:37 -0500

    Update hot-cold-asset.mk

--- firmware/hot-cold-asset.mk
+++ firmware/hot-cold-asset.mk
@@ -1,19 +1,34 @@
-# if "template" is in the make command, do not include static.lib files
-ifneq (,$(findstring template,$(MAKECMDGOALS)))
-ASSET_FILES=$(wildcard static/*)
-else
-ASSET_FILES=$(wildcard static/*) $(wildcard static.lib/*)
-endif
-
-TEMPLATE_FILES+=$(wildcard static/*) $(wildcard firmware/hot-cold-asset.mk)
-
+# Only generate assets if building cold
+ifeq (cold,$(firstword $(MAKECMDGOALS)))
+ASSET_FILES := $(wildcard static/*)
 ASSET_OBJ=$(addprefix $(BINDIR)/, $(addsuffix .o, $(ASSET_FILES)) )
-
-GETALLOBJ=$(sort $(call ASMOBJ,$1) $(call COBJ,$1) $(call CXXOBJ,$1)) $(ASSET_OBJ)
 
 .SECONDEXPANSION:
 $(ASSET_OBJ): $$(patsubst bin/%,%,$$(basename $$@))
 	$(VV)mkdir -p $(BINDIR)/static
-	$(VV)mkdir -p $(BINDIR)/static.lib
 	@echo "ASSET $@"
 	$(VV)$(OBJCOPY) -I binary -O elf32-littlearm -B arm $^ $@
+else
+ASSET_OBJ :=
+endif
+
+
+# if "template" is in the make command, do not include static.lib files
+# ifneq (,$(findstring template,$(MAKECMDGOALS)))
+# ASSET_FILES=$(wildcard static/*)
+# else
+# ASSET_FILES=$(wildcard static/*) $(wildcard static.lib/*)
+# endif
+
+# TEMPLATE_FILES+=$(wildcard static/*) $(wildcard firmware/hot-cold-asset.mk)
+
+# ASSET_OBJ=$(addprefix $(BINDIR)/, $(addsuffix .o, $(ASSET_FILES)) )
+
+# GETALLOBJ=$(sort $(call ASMOBJ,$1) $(call COBJ,$1) $(call CXXOBJ,$1)) $(ASSET_OBJ)
+
+# .SECONDEXPANSION:
+# $(ASSET_OBJ): $$(patsubst bin/%,%,$$(basename $$@))
+# 	$(VV)mkdir -p $(BINDIR)/static
+# 	$(VV)mkdir -p $(BINDIR)/static.lib
+# 	@echo "ASSET $@"
+# 	$(VV)$(OBJCOPY) -I binary -O elf32-littlearm -B arm $^ $@
