de423dbcb8e07bc18882a353adf6c7310002e29c
simon <a4coding@gmail.com>
2025-09-29 13:55:02 -0500

    chassis and driver control update

--- src/global.cpp
+++ src/global.cpp
@@ -10,11 +10,11 @@ pros::Controller masterController(pros::E_CONTROLLER_MASTER);
 // Define VEX Motors
 
 // Define VEX Motor Groups
-pros::MotorGroup left_motor_group({-1, 2, -3}, pros::MotorGearset::blue); // left motors use 600 RPM cartrifges
-pros::MotorGroup right_motor_group({4, -5, 6}, pros::MotorGearset::blue); // right motors use 600 RPM cartridges
+pros::MotorGroup left_motor_group({1, -2,3, -4}, pros::MotorGearset::blue); // left motors use 600 RPM cartrifges
+pros::MotorGroup right_motor_group({5,-6,7,-8}, pros::MotorGearset::blue); // right motors use 600 RPM cartridges
 
 // Define VEX Sensors
-pros::Imu imu(10); 
+pros::Imu imu(17); 
 pros::Rotation horizontal_encoder(20); // horizontal tracking wheel Rotation sensor
 pros::Rotation vertical_encoder(21); // vertical tracking wheel Rotation sensor
 
